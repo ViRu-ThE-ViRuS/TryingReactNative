e491fdf131cc7e1e5fb0efb267c3834723246241
Viraat Chandra <viraat.chandra@hotmail.com>
2018-02-17 15:21:28 +0530

    Basic Automated Testing.

---
+++ __tests__/Profile.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ProfileScreen from '../components/ProfileScreen/ProfileScreen';
+
+test('renders correctly', () => {
+    expect(true).toBe(true)
+})
